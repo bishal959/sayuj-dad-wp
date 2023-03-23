4b6d2c2c30808dc0bb2987a2b7e50219038afbf4
SayujK <105600499+SayujK@users.noreply.github.com>
2023-03-23 20:49:09 +0545

    website-redesign

--- js/sidebar.js
+++
@@ -1,9 +0,0 @@
-function openSidebar() {
-  document.getElementById("sidebar").style.right = "0";
-  document.body.classList.add("no-scroll");
-}
-
-function closeSidebar() {
-  document.getElementById("sidebar").style.right = "-150vh";
-  document.body.classList.remove("no-scroll");
-}
