9161f603e5bf5ef940525576fb430867f6128928
SayujK <105600499+SayujK@users.noreply.github.com>
2023-04-05 19:44:00 +0545

    Delete js directory

--- js/dark-mode-toggle.js
+++
@@ -1,8 +0,0 @@
-// Get the current state of the checkbox from local storage
-var checkbox = document.getElementById("dark-mode-toggle");
-checkbox.checked = JSON.parse(localStorage.getItem("dark-mode-toggle"));
-
-function toggleDarkMode() {
-  // Save the current state of the checkbox to local storage
-  localStorage.setItem("dark-mode-toggle", checkbox.checked);
-}
